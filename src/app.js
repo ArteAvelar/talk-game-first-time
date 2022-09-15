189b4cc318650191552a10363c70f512b0e9223f
ArteAvelar <arte.avelar@gmail.com>
2022-09-15 17:23:43 +0200

    added first change on the h2

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
-function handleClick() {
-  alert("hi");
+function handleClick(event) {
+  event.preventDefault();
+  alert(sentence[1]);
+  let h2 = document.querySelector("h2");
+  h2.innerHTML = sentence[2];
 }
 
 let element = document.querySelector("#newSentenceButton");
